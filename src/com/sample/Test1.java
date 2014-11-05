a707e2487aaccfe234123b66c35d2f44a5c638fb
Dasu94 <dasu94@gmail.com>
2014-11-04 22:23:06 -0800

    new test1 class

---
+++ src/com/sample/Test1.java
@@ -0,0 +1,10 @@
+package com.sample;
+
+/**
+ * Created by Das on 11/4/2014.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+    }
+}
